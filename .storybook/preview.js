e20cf408384275690e55134ab8ee5f7d1dd8cced
Miguel Nogueira <mglsndc@gmail.com>
2021-09-15 17:31:33 -0300

    chore(storybook): wrap stories in theme provider & add google fonts

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,24 @@
+import React from 'react';
+
+import { GlobalStyles } from '../src/styles/globals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../src/styles/theme';
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  ),
+];
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
