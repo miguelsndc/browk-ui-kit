cb49ea43ca5c907949215e1f463bfc3c95dc6566
Miguel Nogueira <mglsndc@gmail.com>
2021-09-15 17:34:03 -0300

    feat: custom theme; custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { GlobalStyles } from './styles/globals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
+
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
